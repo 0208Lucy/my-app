ef705cc1cc184b981704a123648fe41c7cffe16e
0208Lucy <4a9g0906@stust.edu.tw>
2024-05-02 17:35:40 +0800

    0502

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Hello React! 2024.5.2</h1>
-        <h2>4A9G0906 施婉柔</h2>
+        <h2>4A9G0906施婉柔</h2>
         <MySlider color="RED"/>
         <MySlider color="GREEN"/>
         <MySlider color="BLUE"/>
