f69257cf764a90e68e06075a6870b34d3e36f1f4
施婉柔 <4a9g0906@stust.edu.tw>
2024-06-01 02:04:25 +0800

    feat: Add Tic-Tac-Toe game component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 import MyRGBPanel from "./components/rgb-panel/MyRGBPanel";
 import MySlider from "./components/rgb-panel/MySlider";
 import MyCalculator from "./components/calculator/MyCalculator";
+import Board from "./components/tic-tac-toe/Board";
+import TicTacToe from "./components/tic-tac-toe/TicTacToe";
 
 function App() {
   return (
@@ -10,9 +12,12 @@ function App() {
         <h1>Hello React! 2024.5.16</h1>
         <h2>4A9G0906施婉柔</h2>
         <MyRGBPanel />
+
         <h2>第二個元件：MyCalculator</h2>
         <MyCalculator />
+
         <h2>第三個元件：Tic-Tac-Toe Game</h2>
+        <TicTacToe />
       </header>
     </div>
   );
